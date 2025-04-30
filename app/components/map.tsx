6a16732224a104db3c23955707136bba4055336f
ide-an <ideanweb@gmail.com>
2025-05-01 00:49:18 +0900

    地図上にアイコン表示。画像の調整はしたい

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -8,20 +8,56 @@ import ImageLayer from "ol/layer/Image";
 import Static from "ol/source/ImageStatic";
 import { Projection } from "ol/proj";
 import { getCenter } from "ol/extent";
-import { Probe } from "../data/probe";
+import { Probe, ProbePoint } from "../data/probe";
 import { set } from "ol/transform";
-import { LineString } from "ol/geom";
+import { LineString, Point } from "ol/geom";
 import Style from "ol/style/Style";
 import Stroke from "ol/style/Stroke";
 import Vector from "ol/layer/Vector";
 import VectorSrouce from "ol/source/Vector";
+import { Liver } from "../data/liver";
+import Icon from "ol/style/Icon";
+
+const createRouteLineFeature = (probePoints: ProbePoint[], liver: Liver) => {
+  const line = new LineString(probePoints.map((point) => {
+    return [point.x, point.y]; // TODO: 座標の補正
+  }))
+  const feature = new Feature({ geometry: line });
+  feature.setStyle([
+    new Style({
+      stroke: new Stroke({
+        color: "#ffffff",
+        width: 5,
+      }),
+    }),
+    new Style({
+      stroke: new Stroke({
+        color: liver.color,
+        width: 3,
+      }),
+    })
+  ]);
+  return feature;
+};
+const createMarkerFeature = (probePoint: ProbePoint, liver: Liver) => {
+  const point = new Point([probePoint.x, probePoint.y]);
+  const feature = new Feature({ geometry: point });
+  feature.setStyle(new Style({
+    image: new Icon({
+      src: liver.imageUrl,
+      scale: 0.1,
+      anchor: [0.5, 1.5],
+    }),
+  }));
+  return feature;
+}
 
 function Map({
   probes,
   gtaTime,
   showRoute,
   isPlaying,
-}:{
+}: {
   probes: Probe[],
   gtaTime: number,
   showRoute: boolean,
@@ -65,7 +101,7 @@ function Map({
     });
     console.log("map", map);
     const vectorSource = new VectorSrouce({
-        features: []
+      features: []
     });
     map.addLayer(new Vector({
       source: vectorSource,
@@ -77,33 +113,24 @@ function Map({
   }, []);
 
   useEffect(() => {
-    if (!routeVectorSource){
+    if (!routeVectorSource) {
       return;
     }
     routeVectorSource.clear();
     // TODO: probeの描画
-    for(const probe of probes) {
+    for (const probe of probes) {
       const probePoints = probe.probePoints;
-      const points = probePoints
-        .filter((point) => point.t < gtaTime)
-        .map((point) => {
-          return [point.x, point.y];
-        });
-      if (points.length === 0) {
+      const visitedPoints = probePoints.filter((point) => point.t < gtaTime)
+      if (visitedPoints.length === 0) {
         continue;
       }
       // TODO: 次の点を補完で追加する
       // console.log("points", points);  
-      const line = new LineString(points);
-      const feature = new Feature({ geometry: line });
-      feature.setStyle(new Style({
-        stroke: new Stroke({
-          color: 'red',
-          width: 2,
-        }),
-      }));
-      routeVectorSource.addFeature(feature);
+      const routeLineFeature = createRouteLineFeature(visitedPoints, probe.liver);
+      routeVectorSource.addFeature(routeLineFeature);
       // TODO: マーカーの追加
+      const markerFeature = createMarkerFeature(visitedPoints[visitedPoints.length - 1], probe.liver);
+      routeVectorSource.addFeature(markerFeature);
     }
     if (map) {
       map.render();
