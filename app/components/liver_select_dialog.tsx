bab2a41a3f2510e131cecf7f98956cba331d53ac
ide-an <ideanweb@gmail.com>
2025-05-02 17:47:54 +0900

    ライバーからクエリで絞り込む。クエリ入力まわりのパフォーマンスが悪いので要調査

--- app/components/liver_select_dialog.tsx
+++ app/components/liver_select_dialog.tsx
@@ -6,10 +6,12 @@ import {
   DialogBody,
   DialogFooter,
   DialogHeader,
+  Input,
   Typography,
 } from "@material-tailwind/react";
 import { useState } from "react";
 import { Liver } from "../data/liver";
+import livers from "../data/livers.json";
 
 // ライバーカラーのborder。 globals.cssでsafelistに追加しているクラスと対応する
 const toBorderColorClass = (liver: Liver) => {
@@ -23,11 +25,21 @@ export default function LiverSelectDialog({
   selectedLivers: Liver[];
   onSelectedLiversChange: (livers: Liver[]) => void;
 }) {
+    console.time("LiverSelectDialog render");
   const [open, setOpen] = useState(false);
   const [query, setQuery] = useState("");
   const handleOpen = () => {
     setOpen((prev) => !prev);
   };
+console.timeLog("LiverSelectDialog render", "open", open);
+  const filteredLivers = livers.filter((liver) => {
+    if (query === "") return true;
+    if (liver.name.includes(query)) return true;
+    if (liver.tags.some((tag) => tag.includes(query))) return true;
+    return false;
+  });
+  console.timeEnd("LiverSelectDialog render");
+
   return (
     <>
       {/* ライバー選択 */}
@@ -42,22 +54,68 @@ export default function LiverSelectDialog({
         <DialogBody>
           <div className="flex flex-row gap-6">
             {/* ライバーの検索 */}
-            <div className="flex-1">aaaaa</div>
+            <div className="flex-1">
+              <div className="p-1">
+                <Input
+                  label="ライバー名、ジョブなどで検索"
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                />
+              </div>
+              <Button size="sm" color="green">
+                全選択
+              </Button>
+              <div className="overflow-y-auto h-96">
+                {filteredLivers.map((liver) => (
+                  <div key={liver.id} className="flex items-center gap-4">
+                    <div className="flex-none">
+                      <Button size="sm" variant="outlined" color="green">
+                        選択
+                      </Button>
+                    </div>
+                    <Avatar
+                      src={liver.imageUrl}
+                      className={`border-2 ${toBorderColorClass(
+                        liver
+                      )} flex-none`}
+                    />
+                    <div className="grow">
+                      <Typography variant="h6">{liver.name}</Typography>
+                      <div className="flex flex-wrap gap-2">
+                        {liver.tags.map((tag, index) => (
+                          <Chip
+                            value={tag}
+                            key={liver.name + "_" + tag}
+                            size="sm"
+                          />
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
             {/* 選択済みのライバー一覧 */}
             <div className="flex-1">
               <Typography variant="h5" className="mb-4">
                 選択中のライバー
               </Typography>
-              <Button  size="sm" color="red">全解除</Button>
+              <Button size="sm" color="red">
+                全解除
+              </Button>
               <div className="overflow-y-auto h-96">
                 {selectedLivers.map((liver) => (
                   <div key={liver.id} className="flex items-center gap-4">
                     <div className="flex-none">
-                      <Button size="sm" variant="outlined" color="red">解除</Button>
+                      <Button size="sm" variant="outlined" color="red">
+                        解除
+                      </Button>
                     </div>
                     <Avatar
                       src={liver.imageUrl}
-                      className={`border-2 ${toBorderColorClass(liver)} flex-none`}
+                      className={`border-2 ${toBorderColorClass(
+                        liver
+                      )} flex-none`}
                     />
                     <div className="grow">
                       <Typography variant="h6">{liver.name}</Typography>
